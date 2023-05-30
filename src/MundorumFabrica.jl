18644122c90070e2a75cd68795a06d5bd5589a08
zimlit <zimlit2010@gmail.com>
2023-05-29 20:02:59 -0400

    planet physical characteristics

--- src/MundorumFabrica.jl
+++ src/MundorumFabrica.jl
@@ -59,8 +59,6 @@ module MundorumFabrica
         end
     end
 
-    star = Star(1.0)
-
     function Base.show(io::Core.IO, s::Star)
         println(io, "Star with mass $(s.mass) M☉")
         println(io, "Lifespan: $(s.lifespan) billion years")
@@ -73,9 +71,43 @@ module MundorumFabrica
         print(io, "Habitable zone: $(s.HabitableZone[1]) AU to $(s.HabitableZone[2]) AU")
     end
 
-    function activate(app)
-        win = GtkApplicationWindow(app, "Mundorum Fabrica")
+    struct Planet
+        mass::Float64
+        radius::Float64
+        surface_area::Float64
+        volume::Float64
+        density::Float64
+        gravity::Float64
+        axialtilt::Float64
+        rotationperiod::Float64
+        albedo::Float64
+        
+        function Planet(mass::Float64, radius::Float64, axialtilt::Float64, rotationperiod::Float64, albedo::Float64)
+            surface_area = 4 * π * (radius*6371)^2
+            volume = (4/3) * π * (radius*6371)^3
+            density = ((mass*5.972e+24)/volume)/1e+12
+            gravity = mass/radius^2
 
+            return new(mass, radius, surface_area, volume, density, gravity, axialtilt, rotationperiod, albedo)
+        end
+    end
+
+    function Base.show(io::Core.IO, p::Planet)
+        println(io, "Planet with mass $(p.mass) M☉")
+        println(io, "Radius: $(p.radius) R☉")
+        println(io, "Surface area: $(p.surface_area) km²")
+        println(io, "Volume: $(p.volume) km³")
+        println(io, "Density: $(p.density) ρ☉")
+        println(io, "Gravity: $(p.gravity) G")
+        println(io, "Axial tilt: $(p.axialtilt)°")
+        println(io, "Rotation period: $(p.rotationperiod) hours")
+        print(io, "Albedo: $(p.albedo)")
+    end
+
+    star = Star(1.0)
+    planet = Planet(1.0, 1.0, 23.5, 24.0, 0.29)
+
+    function makestarui()
         starbox = GtkGrid()
         starbox.column_spacing = 15
         starbox.margin_top = 20
@@ -85,12 +117,16 @@ module MundorumFabrica
         sb = GtkSpinButton(adjustment, 1, 4)
 
         mass = GtkLabel("Mass (M☉)")
+        Gtk4.markup(mass, "Mass (M<sub>☉</sub>)")
         li_l = GtkLabel("Lifespan (Gyr)")
         r_l = GtkLabel("Radius (R☉)")
+        Gtk4.markup(r_l, "Radius (R<sub>☉</sub>)")
         sa_l = GtkLabel("Surface area (km²)")
         v_l = GtkLabel("Volume (km³)")
         l_l = GtkLabel("Luminosity (L☉)")
+        Gtk4.markup(l_l, "Luminosity (L<sub>☉</sub>)")
         ρ_l = GtkLabel("Density (ρ☉)")
+        Gtk4.markup(ρ_l, "Density (ρ<sub>☉</sub>)")
         t_l = GtkLabel("Temperature (K)")
         hz_l = GtkLabel("Habitable zone (AU)")
         mass.xalign = 0
@@ -138,18 +174,7 @@ module MundorumFabrica
 
         starbox[1, 9] = hz_l
         starbox[2, 9] = hz
-
-        stack = GtkStack()
-        push!(stack, starbox, "Star", "Star")
-        stackswitcher = GtkStackSwitcher()
-        stackswitcher.stack = stack
-
-        vbox = GtkBox(:v)
-        push!(vbox, stackswitcher)
-        push!(vbox, stack)
-
-        push!(win, vbox)
-
+        
         signal_connect(sb, "value-changed") do b
             star = Star(sb.value)
             li.label = Printf.@sprintf("%.3f", star.lifespan)
@@ -161,6 +186,128 @@ module MundorumFabrica
             t.label = Printf.@sprintf("%.3f", star.temperature)
             hz.label = Printf.@sprintf("%.3f", star.HabitableZone[1]) * " to " * Printf.@sprintf("%.3f", star.HabitableZone[2])
         end
+
+        return starbox
+    end
+
+    function makeplanetui()
+        vbox = GtkBox(:v)
+
+        stack = GtkStack()
+        stack.transition_type = Gtk4.StackTransitionType_SLIDE_LEFT_RIGHT
+        stackswitcher = GtkStackSwitcher()
+        stackswitcher.stack = stack
+
+        physicalcharacteristicsbox = GtkGrid()
+        physicalcharacteristicsbox.column_spacing = 15
+        physicalcharacteristicsbox.margin_top = 20
+        physicalcharacteristicsbox.halign = Gtk4.Align_CENTER
+
+        adjustment = GtkAdjustment(1, 0, 4767, 0.1, 10, 0)
+        sb_mass = GtkSpinButton(adjustment, 1, 4)
+
+        adjustment = GtkAdjustment(1, 0, 25, 0.1, 10, 0)
+        sb_radius = GtkSpinButton(adjustment, 1, 4)
+
+        adjustment = GtkAdjustment(23.5, 0, 360, 0.1, 10, 0)
+        sb_axialtilt = GtkSpinButton(adjustment, 1, 4)
+
+        adjustment = GtkAdjustment(24, 0, 10000, 0.1, 10, 0)
+        sb_rotationperiod = GtkSpinButton(adjustment, 1, 4)
+
+        adjustment = GtkAdjustment(0.29, 0, 1, 0.01, 10, 0)
+        sb_albedo = GtkSpinButton(adjustment, 1, 4)
+
+        mass = GtkLabel("m")
+        Gtk4.markup(mass, "Mass (M<sub>⊕</sub>)")
+        radius = GtkLabel("r")
+        Gtk4.markup(radius, "Radius (R<sub>⊕</sub>)")
+        sa_l = GtkLabel("Surface area (km²)")
+        v_l = GtkLabel("Volume (km³)")
+        ρ_l = GtkLabel("Density (ρ)")
+        Gtk4.markup(ρ_l, "Density (gm/cm<sup>3</sup>)")
+        g_l = GtkLabel("Gravity (g)")
+        a_l = GtkLabel("Axial tilt (°)")
+        p_l = GtkLabel("Rotation period (earth hours)")
+        albedo_l = GtkLabel("Bond albedo")
+
+        sa = GtkLabel(Printf.@sprintf("%.3e", planet.surface_area))
+        v = GtkLabel(Printf.@sprintf("%.3e", planet.volume))
+        ρ = GtkLabel(Printf.@sprintf("%.3f", planet.density))
+        g = GtkLabel(Printf.@sprintf("%.3f", planet.gravity))
+
+        physicalcharacteristicsbox[1, 1] = mass
+        physicalcharacteristicsbox[2, 1] = sb_mass
+
+        physicalcharacteristicsbox[1, 2] = radius
+        physicalcharacteristicsbox[2, 2] = sb_radius
+
+        physicalcharacteristicsbox[1, 3] = sa_l
+        physicalcharacteristicsbox[2, 3] = sa
+
+        physicalcharacteristicsbox[1, 4] = v_l
+        physicalcharacteristicsbox[2, 4] = v
+
+        physicalcharacteristicsbox[1, 5] = ρ_l
+        physicalcharacteristicsbox[2, 5] = ρ
+
+        physicalcharacteristicsbox[1, 6] = g_l
+        physicalcharacteristicsbox[2, 6] = g
+
+        physicalcharacteristicsbox[1, 7] = a_l
+        physicalcharacteristicsbox[2, 7] = sb_axialtilt
+
+        physicalcharacteristicsbox[1, 8] = p_l
+        physicalcharacteristicsbox[2, 8] = sb_rotationperiod
+
+        physicalcharacteristicsbox[1, 9] = albedo_l
+        physicalcharacteristicsbox[2, 9] = sb_albedo
+
+        function compute(w)
+            planet = Planet(sb_mass.value, sb_radius.value, sb_axialtilt.value, sb_rotationperiod.value, sb_albedo.value)
+            sa.label = Printf.@sprintf("%.3e", planet.surface_area)
+            v.label = Printf.@sprintf("%.3e", planet.volume)
+            ρ.label = Printf.@sprintf("%.3f", planet.density)
+            g.label = Printf.@sprintf("%.3f", planet.gravity)
+        end
+
+        signal_connect(compute, sb_mass, "value-changed")
+        signal_connect(compute, sb_radius, "value-changed")
+        signal_connect(compute, sb_axialtilt, "value-changed")
+        signal_connect(compute, sb_rotationperiod, "value-changed")
+        signal_connect(compute, sb_albedo, "value-changed")
+        
+        orbitalcharacteristicsbox = GtkGrid()
+
+        atmosphericcharacteristicsbox = GtkGrid()
+
+        push!(stack, physicalcharacteristicsbox, "Physical Characteristics", "Physical Characteristics")
+        push!(stack, orbitalcharacteristicsbox, "Orbital Characteristics", "Orbital Characteristics")
+        push!(stack, atmosphericcharacteristicsbox, "Atmospheric Characteristics", "Atmospheric Characteristics")
+        push!(vbox, stackswitcher)
+        push!(vbox, stack)
+
+        return vbox
+    end
+
+    function activate(app)
+        win = GtkApplicationWindow(app, "Mundorum Fabrica")
+
+        starbox = makestarui()
+        planetbox = makeplanetui()
+
+        stack = GtkStack()
+        stack.transition_type = Gtk4.StackTransitionType_SLIDE_LEFT_RIGHT
+        push!(stack, starbox, "Star", "Star")
+        push!(stack, planetbox, "Planet", "Planet")
+        stackswitcher = GtkStackSwitcher()
+        stackswitcher.stack = stack
+
+        vbox = GtkBox(:v)
+        push!(vbox, stackswitcher)
+        push!(vbox, stack)
+
+        push!(win, vbox)
 
         show(win)
     end
